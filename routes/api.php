0de91002e15d5c7eab4d63f43842e3171d8ee0ae
inrafilus <inrafilus@rekamy.com>
2023-06-06 18:18:31 +0800

    env

--- routes/api.php
+++ routes/api.php
@@ -14,6 +14,11 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
+Route::get('/pijan', fn () => response(['name' => 'Hafizan']));
+
+Route::middleware(['auth:sanctum'])->group(function() {
+
+    Route::get('/user', fn (Request $request) => $request->user());
+
 });
+
