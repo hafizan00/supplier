da9dbdc188acf6474c0426ac3b0751452a936c1c
hafizan00 <hafizanabdila26@gmail.com>
2023-06-08 17:44:37 +0800

    ss

--- index.php
+++ index.php
@@ -7,7 +7,34 @@ if ($session->isUserLoggedIn(true)) {
 ?>
 <?php include_once('layouts/header.php'); ?>
 
-<div class="login-page" style="padding-top: 400px;">
+<div class="row">
+  <div class="col-6 nav">
+    <nav class="navbar navbar-default">
+      <div class="container-fluid">
+        <!-- Brand and toggle get grouped for better mobile display -->
+        <div class="navbar-header">
+          <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
+            <span class="sr-only">Toggle navigation</span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+          </button>
+          <a class="navbar-brand" href="#">Brand</a>
+        </div>
+
+        <!-- Collect the nav links, forms, and other content for toggling -->
+        <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+          <ul class="nav navbar-nav navbar-right">
+            <li><a href="#">Link</a></li>
+          </ul>
+        </div><!-- /.navbar-collapse -->
+      </div><!-- /.container-fluid -->
+    </nav>
+  </div>
+</div>
+
+<div class="login-page" style="padding-top: 360px;">
+
   <div class="login-box">
     <div class="text-center">
       <h3 style="color:#fff ;">MY Supplier</h3>
@@ -204,6 +231,9 @@ if ($session->isUserLoggedIn(true)) {
     animation-delay: .75s
   }
 
+  .nav {
+    margin-right: 250px;
+  }
   @keyframes btn-anim4 {
     0% {
       bottom: -100%;
